21f0870d6a79dbfc2947ce964681e75045ae5c4d
Isaac André <zack597games@gmail.com>
2020-08-02 20:18:07 -0300

    commit full pack

---
+++ pacote download/Primeiro Projeto/JS/Script.js
@@ -0,0 +1,9 @@
+$('.Slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+})
